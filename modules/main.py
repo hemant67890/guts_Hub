cbc477a4822b16b9771685824b7c0dfffca5dcc2
MARCO-015 <Charlie.aimers07@gmail.com>
2025-02-04 21:46:13 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -21,7 +21,7 @@ from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 from pyrogram.types.messages_and_media import message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-OWNER = int(os.environ.get("OWNER", 7548265642))
+OWNER = int(os.environ.get("OWNER", "7548265642"))
 
 bot = Client(
     "bot",
