808b48b1a6cdb6b0a708ddde375d02de0b923cda
hemant67890 <godarasubhash337@gmail.com>
2025-02-25 21:08:55 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 api_id    = os.environ.get("api_id", "25579552")
 api_hash  = os.environ.get("api_hash", "ac24e438ff9a0f600cf3283e6d60b1aa")
-bot_token = os.environ.get("bot_token", "7764318516:AAEGWG5d-qusaA0c_YWo8Cnjr6bRwc8px5w")
+bot_token = os.environ.get("bot_token", "7705076607:AAHPwB2ifTJg2DCPynWRYvleA3IAaKwthwA")
 pwtoken = os.getenv("pwtoken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzgyNDQwNTIuOTc0LCJkYXRhIjp7Il9pZCI6IjY0YTEwMTJlMWNmOWI0MDAxOGI1MTQxZCIsInVzZXJuYW1lIjoiNjIwNzM4NDEwNyIsImZpcnN0TmFtZSI6IkFqIFB5dGhvbiIsImxhc3ROYW1lIjoiSkhBIiwib3JnYW5pemF0aW9uIjp7Il9pZCI6IjVlYjM5M2VlOTVmYWI3NDY4YTc5ZDE4OSIsIndlYnNpdGUiOiJwaHlzaWNzd2FsbGFoLmNvbSIsIm5hbWUiOiJQaHlzaWNzd2FsbGFoIn0sImVtYWlsIjoiYWpweXRob24yMjJAZ21haWwuY29tIiwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzM3NjM5MjUyfQ.EtWQ3feCPSfcVojPdcl1WAYuvdAYXumpVdzPEcAzjX0")
-AUTH = "7548265642"
+AUTH = "7246728595"
 #Bot Username = "MARCO_015"
